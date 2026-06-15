e5ca039a9e7f95b56b4f8bff3015de443174bf63
Axel Barbellion <axelbarbellion@gmail.com>
2026-06-15 16:14:29 +0200

    feat:supression auth + persistance session

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use App\Livewire\Game\Classic;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-});
-
 Route::livewire('game/classic', Classic::class)->name('game.classic');
 
 require __DIR__ . '/settings.php';
